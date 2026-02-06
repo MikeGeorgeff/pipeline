a802c05431f32845176e2c34ad47d66f692bf87e
Mike Georgeff <mike@georgeff.co>
2026-02-06 04:39:46 -0600

    cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,49 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    php-dev.url = "github:MikeGeorgeff/php-dev-flake";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    ext = ({ enabled, all }: enabled ++ [ all.redis ]);
-
-    # Helper to create shells for a given PHP version
-    mkPhpShells = name: php: {
-      "${name}" = pkgs.mkShell {
-        name = "${name} Environment";
-        buildInputs = [ php php.packages.composer ];
-      };
-
-      "${name}-update" = pkgs.mkShell {
-        buildInputs = [ php php.packages.composer ];
-        shellHook = ''
-          composer update
-          exit
-        '';
-      };
-
-      "${name}-test" = pkgs.mkShell {
-        buildInputs = [ php php.packages.composer ];
-        shellHook = ''
-          composer test
-          exit
-        '';
-      };
-
-      "${name}-analyze" = pkgs.mkShell {
-        buildInputs = [ php php.packages.composer ];
-        shellHook = ''
-          composer analyze
-          exit
-        '';
-      };
-
-    };
-  in
+  outputs = { self, php-dev }:
   {
-    devShells.x86_64-linux =
-      (mkPhpShells "php82" (pkgs.php82.withExtensions ext)) //
-      (mkPhpShells "php84" (pkgs.php.withExtensions ext));
+    devShells = php-dev.devShells;
   };
 }
